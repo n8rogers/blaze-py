a380ae035d2985a58e7449f2bf198ab2cef9c44e
Nate Rogers <n8rogers@outlook.com>
2017-10-14 15:32:11 -0400

    setting pin

--- blaze.py
+++ blaze.py
@@ -6,18 +6,18 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
-GPIO.setup(25, GPIO.IN)
+# GPIO.setup(23, GPIO.IN)
+# GPIO.setup(24, GPIO.IN)
+GPIO.setup(26, GPIO.IN)
 
 while True:
-    if (GPIO.input(23) == False):
-        os.system('mpg123 -q binary-language-moisture-evaporators.mp3 &')
+    # if (GPIO.input(23) == False):
+    #     os.system('mpg123 -q binary-language-moisture-evaporators.mp3 &')
 
-    if (GPIO.input(24) == False):
-        os.system('mpg123 -q power-converters.mp3 &')
+    # if (GPIO.input(24) == False):
+    #     os.system('mpg123 -q power-converters.mp3 &')
 
-    if (GPIO.input(25)== False):
-        os.system('mpg123 -q vader.mp3 &')
+    if (GPIO.input(26)== False):
+        os.system('mpg123 -q blaze_theme.mp3 &')
 
     sleep(0.1);
