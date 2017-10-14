25c53dab21d53169e631f3e556f54a041f32f3c9
Nate Rogers <n8rogers@outlook.com>
2017-10-14 15:37:25 -0400

    removing background process

--- blaze.py
+++ blaze.py
@@ -10,6 +10,8 @@ GPIO.setmode(GPIO.BCM)
 # GPIO.setup(24, GPIO.IN)
 GPIO.setup(26, GPIO.IN)
 
+sound
+
 while True:
     # if (GPIO.input(23) == False):
     #     os.system('mpg123 -q binary-language-moisture-evaporators.mp3 &')
@@ -18,6 +20,6 @@ while True:
     #     os.system('mpg123 -q power-converters.mp3 &')
 
     if (GPIO.input(26)== False):
-        os.system('mpg123 -q blaze_theme.mp3 &')
+        os.system('mpg123 -q blaze_theme.mp3')
 
     sleep(0.1);
