ed2854459bf9e1ef783598d61265d9b972c44368
Nate Rogers <n8rogers@outlook.com>
2017-10-15 16:14:18 -0400

    adding logic, ignoring mp3

--- blaze.py
+++ blaze.py
@@ -1,25 +1,58 @@
 #!/usr/bin/env python
 
-import os
+import os, pygame
 from time import sleep
+from random import randint
+
+pygame.mixer.init()
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-# GPIO.setup(23, GPIO.IN)
-# GPIO.setup(24, GPIO.IN)
-GPIO.setup(26, GPIO.IN)
+input1 = 26
+input2 = 25
+item = 0
+playing = false;
 
-sound
+soundsList = []
+for file in os.listdir("."):
+	if file.endswith(".mp3"):
+		soundsList.append(file)
+
+def getRandomSound():
+	listSize = len(soundsList)
+	if(listSize > 0):
+		return soundsList[randint(0,listSize-1)]
+	else:
+		return null
+
+
+def getNextSound():
+	global item 
+	item += 1
+	if(item > len(soundsList) - 1):
+		item = 0;
+	
+	return soundsList[item]
+
+def playSound():
+	if(playing):
+		pygame.mixer.stop()
+		playing = false;
+	else:
+		pygame.mixer.music.load(getNextSound())
+		pygame.mixer.music.play()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(input1, GPIO.IN)
+#GPIO.setup(input2, GPIO.IN)
+
 
 while True:
-    # if (GPIO.input(23) == False):
-    #     os.system('mpg123 -q binary-language-moisture-evaporators.mp3 &')
 
-    # if (GPIO.input(24) == False):
-    #     os.system('mpg123 -q power-converters.mp3 &')
+    if (GPIO.input(input1)== False):
+    	playSound()
 
-    if (GPIO.input(26)== False):
-        os.system('mpg123 -q blaze_theme.mp3')
+    # if (GPIO.input(input2)== False):
+    #     os.system('mpg123 -q blaze_theme.mp3')
 
     sleep(0.1);
